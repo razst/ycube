26cef47f7a14f78023bf69fa9776837782f0e1c5
Raz <raz_st@hotmail.com>
2020-06-07 16:04:43 +0000

    transponder RSSI, min beacon intravl

--- GlobusSatProject/src/utils.h
+++ GlobusSatProject/src/utils.h
@@ -18,6 +18,7 @@
 #define TRXVU_MUTE_TOO_LONG    	-202
 #define TRXVU_IDLE_TOO_LONG    	-203
 #define TRXVU_IDLE_WHILE_TRANSPONDER -205
+#define BEACON_INTRAVL_TOO_SMALL -206
 
 #define MAX_ERRORS       				 20 // max errors we want to log from the same type toghether
 #define MAX_TIME_BETWEEN_ERRORS          2 // max seconds we allow between errors
