745307a2bf34c839da943d16e7e9801e48e999c4
Raz <raz_st@hotmail.com>
2024-07-15 16:13:49 +0300

    update conflict

--- GlobusSatProject/src/TestingDemos/FileSystemTestingDemo.c
+++ GlobusSatProject/src/TestingDemos/FileSystemTestingDemo.c
@@ -51,16 +51,12 @@ int TestListFiles(char* path, int level) {
 				c++;
 				if (find.attr&F_ATTR_DIR) {
 					printf (" dir \n\r");
-<<<<<<< HEAD
 
 					char temp[20] = {0};
 					memcpy(temp, path, strlen(path) -3);
 					sprintf(temp, "%s%s/*.*", temp, filename);
 					TestListFiles(temp, level+1);
 
-=======
-					TestListFiles(sprintf("%s/%s", filename, path));
->>>>>>> ed0242eedbebc9b16069da7e3391c0fafb91a8da
 				} else {
 					printf (" size %d\n\r",find.filesize);
 				}
