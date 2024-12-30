519548bc3337963b768adc715975c2224cab395d
Raz <raz_st@hotmail.com>
2024-12-30 19:29:06 +0200

    Updated error codes (were duplicates)

--- GlobusSatProject/src/utils.h
+++ GlobusSatProject/src/utils.h
@@ -12,24 +12,24 @@
 #include <hal/Timing/Time.h>
 
 
+#define TRXVU_TRANSPONDER_TOO_LONG		-199
 #define E_CANT_TRANSMIT					-200
 #define E_TOO_EARLY_4_BEACON			-201
-#define E_INVALID_PARAMETERS			-204
 #define TRXVU_MUTE_TOO_LONG				-202
 #define TRXVU_IDLE_TOO_LONG				-203
-#define TRXVU_TRANSPONDER_TOO_LONG		-199
+#define E_INVALID_PARAMETERS			-204
 #define TRXVU_IDLE_WHILE_TRANSPONDER	-205
-#define TRXVU_TRANSPONDER_WHILE_MUTE	-211
-#define TRXVU_IDEL_WHILE_MUTE			-212
 #define BEACON_INTRAVL_TOO_SMALL		-206
 #define SPL_DATA_TOO_BIG				-207
 #define INVALID_TLM_TYPE				-208
 #define INVALID_IMG_TYPE				-209
 #define INFO_MSG						-210
-#define PAYLOAD_TIMEOUT					-211
-#define PAYLOAD_FALSE_OPERATION			-212
-#define PAYLOAD_IS_DEAD			-213
-#define E_UNAUTHORIZED					-240 ///< Passcode or id incorrect
+#define TRXVU_TRANSPONDER_WHILE_MUTE	-211
+#define TRXVU_IDEL_WHILE_MUTE			-212
+#define PAYLOAD_TIMEOUT					-213
+#define PAYLOAD_FALSE_OPERATION			-214
+#define PAYLOAD_IS_DEAD					-215
+#define E_UNAUTHORIZED					-216 ///< Passcode or id incorrect
 
 
 
