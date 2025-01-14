efa1be09de1eca78f078bdcc3d29095c9ab6ebec
Raz <raz_st@hotmail.com>
2025-01-14 21:12:12 +0200

    Ver2 release bin

--- GlobusSatProject/src/SubSystemModules/Communication/TRXVU.h
+++ GlobusSatProject/src/SubSystemModules/Communication/TRXVU.h
@@ -18,7 +18,7 @@
 #define MAX_IDLE_TIME 		(2400) 	///< max IDLE duration will be 20 minutes = 120 *20 [sec]
 #define MIN_BEACON_INTRAVL	5 // min of 5 sec between beacons
 
-#define BEACON_SPL_ID	0x0CFFFFFF
+#define BEACON_SPL_ID	0x13FFFFFF
 
 #define TX_FREQUENCY  436400
 #define RX_FREQUENCY  145970
